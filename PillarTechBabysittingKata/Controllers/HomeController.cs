54086d81a5f76fc564a6ea19bae6a49d2cd28aec
myerscody16 <myerscody16@live.com>
2019-09-28 16:36:11 -0400

    Create and confirm appointment pages, working on datetime

--- PillarTechBabysittingKata/Controllers/HomeController.cs
+++ PillarTechBabysittingKata/Controllers/HomeController.cs
@@ -10,6 +10,12 @@ namespace PillarTechBabysittingKata.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BabysittingDbContext _context;
+
+        public HomeController (BabysittingDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -19,6 +25,35 @@ namespace PillarTechBabysittingKata.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult CreateAppointment(Appointments newAppointment)
+        {
+            //List<Appointments> appointments = _context.Appointments.ToList();
+            //_context.Add(newAppointment);
+            //_context.SaveChanges();
+            return RedirectToAction("ConfirmPage", newAppointment);
+        }
+        public IActionResult ConfirmPage(Appointments newAppointment)
+        {
+            return View(newAppointment);
+        }
+        public IActionResult AddAppointment(Appointments newAppointment)
+        {
+            List<Appointments> appointments = _context.Appointments.ToList();
+            _context.Add(newAppointment);
+            _context.SaveChanges();
+            return RedirectToAction("ListOfAppointments");
+        }
+        public IActionResult ListOfAppointments()
+        {
+            List<Appointments> allAppointments = _context.Appointments.ToList();
+            return View();
+        }
+        //public IActionResult ListFamilyAppointment(Appointments newAppointment)
+        //{
+        //    List<Appointments> allAppointments = _context.Appointments.Where(u => u.FamilyId == newAppointment.FamilyId).ToList();//write test to make sure this works
+        //    return View(allAppointments);
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
